6853718ad7c718343b5645973e28ae7319dded95
Dakota Grusak <dgrusak@trinity.edu>
2019-11-13 10:04:38 -0600

    Changing env vars used

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # curl --list-only -u $USER:$PASS ftp://$HOST
 # Upload html
-curl --ftp-create-dirs -T out/resume.pdf.html -u $USER:$PASS ftp://$HOST/$DIRECTORY/index.html
+curl --ftp-create-dirs -T out/resume.pdf.html -u $FTP_USER:$FTP_PASS ftp://$FTP_HOST/$FTP_DIRECTORY/index.html
 # Upload PDF
-curl --ftp-create-dirs -T out/resume.pdf -u $USER:$PASS ftp://$HOST/$DIRECTORY/docs/CV.pdf
+curl --ftp-create-dirs -T out/resume.pdf -u $FTP_USER:$FTP_PASS ftp://$FTP_HOST/$FTP_DIRECTORY/docs/CV.pdf
